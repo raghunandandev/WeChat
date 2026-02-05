a67c433ccb4e35073ae86397976c924ff3746f3a
raghunandandev <raghunandandevsingh@gmail.com>
2026-02-05 19:33:24 +0530

    Working Code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 import express from "express";
 import { createServer } from "node:http";
 
@@ -23,7 +25,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect(`${process.env.MONGO_URI}`)
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
